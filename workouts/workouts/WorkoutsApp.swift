ef1e8726b2726ba8dac8cbb6bf16093c0e09d188
fraander <57777918+fraander@users.noreply.github.com>
2023-01-02 18:24:09 -0800

    Install firebase

--- workouts/workouts/WorkoutsApp.swift
+++ workouts/workouts/WorkoutsApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
-struct workoutsApp: App {
+struct WorkoutsApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
